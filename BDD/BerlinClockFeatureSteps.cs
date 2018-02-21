6cf857d77049337b6ffeff56f80e63715cead93c
Edgar Hakobyan <edgarhakobyan@outlook.com>
2018-02-21 08:40:48 +0100

    BerlinClockFix

--- BDD/BerlinClockFeatureSteps.cs
+++ BDD/BerlinClockFeatureSteps.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 using TechTalk.SpecFlow;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Linq;
 
 namespace BerlinClock
 {
     [Binding]
     public class TheBerlinClockSteps
     {
-        private ITimeConverter berlinClock = new TimeConverter();
-        private String theTime;
+        private ITimeConverter berlinClock = new BerlinClockConverter();
+        private TimeSpan time;
 
-        
         [When(@"the time is ""(.*)""")]
-        public void WhenTheTimeIs(string time)
+        public void WhenTheTimeIs(string timeToConvert)
         {
-            theTime = time;
+            time = timeToConvert == "24:00:00" ? TimeSpan.FromDays(1) : TimeSpan.Parse(timeToConvert);
         }
-        
+
         [Then(@"the clock should look like")]
         public void ThenTheClockShouldLookLike(string theExpectedBerlinClockOutput)
         {
-            Assert.AreEqual(berlinClock.convertTime(theTime), theExpectedBerlinClockOutput);
+            Assert.AreEqual(berlinClock.Convert(time), theExpectedBerlinClockOutput);
         }
-
     }
 }
---
+++ Classes/BerlinClockConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BerlinClock
+{
+    public class BerlinClockConverter : ITimeConverter
+    {
+        public string Convert(TimeSpan time)
+        {
+            if (time < TimeSpan.Zero || time > TimeSpan.FromDays(1))
+            {
+                throw new ArgumentOutOfRangeException(nameof(time));
+            }
+
+            var yellowLightState = GetYellowLightsState(time.Seconds);
+            var hoursRows = GetLightsForHours(time.Days * 24 + time.Hours);
+            var minutesRows = GetLightsForMinutes(time.Minutes);
+
+            return string.Join(Environment.NewLine, yellowLightState, hoursRows, minutesRows);
+        }
+
+        private string GetYellowLightsState(int seconds)
+        {
+            return seconds % 2 == 0 ? "Y" : "O";
+        }
+
+        private string GetLightsForHours(int hours)
+        {
+            var firstRow = new string('R', hours / 5)
+                .PadRight(4, 'O');
+            var secondRow = new string('R', hours % 5)
+                .PadRight(4, 'O');
+            return string.Join(Environment.NewLine, firstRow, secondRow);
+        }
+
+        private string GetLightsForMinutes(int minutes)
+        {
+            var firstRow = new string('Y', minutes / 5)
+                .PadRight(11, 'O')
+                .Replace("YYY", "YYR");
+            var secondRow = new string('Y', minutes % 5)
+                .PadRight(4, 'O');
+            return string.Join(Environment.NewLine, firstRow, secondRow);
+        }
+    }
+}
